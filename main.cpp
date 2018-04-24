fc56cf6e35e00836e0412782856b920848d3766f
Renata <virginia080@gmail.com>
2018-04-24 16:38:49 +0300

    chnges

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
  
-QPushButton button("Hello, World!");
+QPushButtonhhh button("Hello, World!");
 button.resize(200, 60);
 button.show( );
  
